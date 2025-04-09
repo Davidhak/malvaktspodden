ea808860f54bd9303c116b4e7401c84a148ccb70
David <Davidhak@hotmail.com>
2025-04-09 15:32:35 +0200

    loadMore fix

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const burgerMenu = document.querySelector('.hamburger-nav');
 let loadMoreBtn = document.querySelector('.load-more');
-let currentItem = 3;
+let currentItem = 4;
 
 function showBurgerMenu() {
   burgerMenu.style.display = 'flex';
@@ -16,10 +16,10 @@ function openPage() {
 
 loadMoreBtn.onclick = () =>{
   let cards = [...document.querySelectorAll('.work-container .row .outer-card')];
-  for (var i = currentItem; i < currentItem + 3; i++) {
+  for (var i = currentItem; i < cards.length; i++) {
     cards[i].style.display = 'inline-block';
+    currentItem += 4;
   }
-  currentItem += 3;
 
   if (currentItem >= cards.length) {
     loadMoreBtn.style.display = 'none';
